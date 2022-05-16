c1524e12e6939710ce7316d4305932262943544c
Cyrex23 <45338293+Cyrex24@users.noreply.github.com>
2022-05-16 03:47:07 +0100

    dependência removida

--- rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,6 +1,5 @@
 package com.example.restservice;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
